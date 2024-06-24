38d60fb3dd703c9006c93426ff6ecb3e02081409
Emir Mutlu <emirmutlu169@icloud.com>
2024-06-24 15:13:36 +0300

    added profile delete function

--- src/components/ProfileList.tsx
+++ src/components/ProfileList.tsx
@@ -9,6 +9,8 @@ interface Profile {
 
 const ProfileList: React.FC = () => {
   const [profiles, setProfiles] = useState<Profile[]>([]);
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [deleteIndex, setDeleteIndex] = useState<number | null>(null); // Profil silme işlemi için index
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -19,6 +21,36 @@ const ProfileList: React.FC = () => {
 
   const handleProfileCreate = () => {
     navigate('/create-profile');
+  };
+
+  const handleProfileDelete = () => {
+    if (deleteIndex !== null) {
+      const updatedProfiles = [...profiles];
+      updatedProfiles.splice(deleteIndex, 1);
+      const currentUser = JSON.parse(localStorage.getItem('currentUser') || '{}');
+      localStorage.setItem(currentUser.email + '_profiles', JSON.stringify(updatedProfiles));
+      setProfiles(updatedProfiles);
+      setDeleteIndex(null); // Index'i sıfırla
+      setShowModal(false); // Modalı kapat
+    }
+  };
+
+  const openModal = (index: number) => {
+    setDeleteIndex(index);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const handleProfileItemClick = (profile: Profile) => {
+    navigate('/movies', { state: { profile } });
+  };
+
+  const handleDeleteButtonClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, index: number) => {
+    event.stopPropagation(); // Olayın yayılmasını engelle
+    openModal(index);
   };
 
   return (
@@ -32,15 +64,30 @@ const ProfileList: React.FC = () => {
       ) : (
         <div className="profiles-grid">
           {profiles.map((profile, index) => (
-            <div key={index} className="profile-item" onClick={() => navigate('/movies', { state: { profile } })}>
+            <div key={index} className="profile-item" onClick={() => handleProfileItemClick(profile)}>
               <img src={profile.profilePic} alt="Profile" />
               <p>{profile.nickname}</p>
+              <button className="delete-profile-button" onClick={(e) => handleDeleteButtonClick(e, index)}>
+                Delete
+              </button>
             </div>
           ))}
         </div>
       )}
       {profiles.length > 0 && <button onClick={handleProfileCreate}>Create Profile</button>}
       <button onClick={() => navigate(-1)}>Back</button>
+
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <p>Are you sure you want to delete this profile?</p>
+            <div className="modal-buttons">
+              <button onClick={handleProfileDelete}>Yes</button>
+              <button onClick={closeModal}>Cancel</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
